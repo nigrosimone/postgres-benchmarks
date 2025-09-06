fee8145f4a754558838884cc8a2f45d90cf3daa6
Nigro Simone <nigro.simone@gmail.com>
2025-09-06 09:07:26 +0200

    chore: add postgres-unsafe

--- index.js
+++ index.js
@@ -1,55 +1,54 @@
-import { run, bench } from 'mitata';
-import pg from 'pg';
-import postgres from 'postgres';
+import { run, bench } from "mitata";
+import pg from "pg";
+import postgres from "postgres";
 
 const { native } = pg;
 
 const pgNative = new native.Pool({
-    max: process.env.PGMAX, 
-    hostname: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+  max: process.env.PGMAX,
+  hostname: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 });
 
 const pgVanilla = new pg.Pool({
-    max: process.env.PGMAX,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+  max: process.env.PGMAX,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 });
 
 const sql = postgres({
-    max: process.env.PGMAX,
-    host: process.env.PGHOST,
-    port: process.env.PGPORT,
-    database: process.env.PGDATABASE,
-    user: process.env.PGUSER,
-    password: process.env.PGPASSWORD,
+  max: process.env.PGMAX,
+  host: process.env.PGHOST,
+  port: process.env.PGPORT,
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
 });
 
-
-bench('pg-native', () => {
-    pgNative.query({ text: `select 1 as x`, name: "foo"});
-});
+bench("pg-native", () =>
+  pgNative.query({ text: `select 1 as x`, name: "foo" })
+);
 
 if (global.gc) global.gc();
 
-bench('postgres', () => {
-    sql`select 1 as x`;
-});
+bench("postgres", () => sql`select 1 as x`);
 
 if (global.gc) global.gc();
 
-bench('pg', () => {
-    pgVanilla.query({ text: `select 1 as x`, name: "foo" });
-});
+bench("pg", () => pgVanilla.query({ text: `select 1 as x`, name: "foo" }));
+
+if (global.gc) global.gc();
+
+bench("postgres-unsafe", () => sql.unsafe(`select 1 as x`));
 
 await run({
-    format: 'mitata',
-    colors: true,
-    throw: true
+  format: "mitata",
+  colors: true,
+  throw: true,
 });
